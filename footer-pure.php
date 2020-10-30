42d515737419e5b57cfc9b92f99e29eb81990cc1
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-10-30 19:57:10 +0330

    add footer pure

---
+++ footer-pure.php
@@ -0,0 +1,6 @@
+
+<?php wp_footer(); ?>
+<script src="<?= get_template_directory_uri() ?>/js/instantclick.min.js" data-no-instant></script>
+<script data-no-instant>InstantClick.init();</script>
+</body>
+</html>
