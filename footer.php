a29d578fb05035fce5fba3bda02f4974b7d2ca0a
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-10-30 19:57:57 +0330

    update footer

--- footer.php
+++ footer.php
@@ -1,35 +1,36 @@
 <footer id="footer">
     <div class="container">
-        <div class="divider">
-            <span><?php _e('Categories'); ?></span>
+        <div>
+            <div class="divider">
+                <span><?php _e('Categories'); ?></span>
+            </div>
+            <ul>
+                <?php
+                foreach (get_categories() as $category) { ?>
+                    <li class="brackets">
+                        <a href="<?php echo get_category_link($category->term_id) ?>"><?php echo $category->name ?></a>
+                    </li>
+                <?php } ?>
+            </ul>
         </div>
-        <ul>
-            <?php
-            foreach (get_categories() as $category) { ?>
-                <li class="brackets">
-                    <a href="<?php echo get_category_link($category->term_id) ?>"><?php echo $category->name ?></a>
-                </li>
-            <?php } ?>
-        </ul>
-        <div class="divider">
-            <span><?php _e('Tags') ?></span>
+        <div>
+            <div class="divider">
+                <span><?php _e('Tags') ?></span>
+            </div>
+            <div id="cloud-tags">
+                <?php $args = array(
+                    'smallest'  => 8,
+                    'largest'   => 12,
+                    'unit'      => 'pt',
+                    'number'    => 99,
+                    'format'    => 'flat',
+                    'orderby'   => 'name',
+                    'link'      => 'view',
+                    'taxonomy'  => 'post_tag',
+                ); ?>
+                <?php wp_tag_cloud( $args ); ?>
+            </div>
         </div>
-        <div id="cloud-tags">
-            <?php $args = array(
-                'smallest'  => 8,
-                'largest'   => 12,
-                'unit'      => 'pt',
-                'number'    => 99,
-                'format'    => 'flat',
-                'orderby'   => 'name',
-                'link'      => 'view',
-                'taxonomy'  => 'post_tag',
-            ); ?>
-            <?php wp_tag_cloud( $args ); ?>
-        </div>
-        
-        <div class="divider"></div>
-        <ul>
     <?php       
     // header links from Theme panel 
     $first_footer_link = get_option('first_footer_url');
@@ -81,13 +82,12 @@
     <?php } ?>
     </ul>
 
-    <br>
     <?= get_option('footer_text') ?>
     <?php if ( get_option('footer_text') != null && get_option('footer_text') != '' ) : ?>
         <div class="divider"></div>
     <?php endif; ?>
     <p id="copyright-stuff">
-        <a href="https://github.com/TheYahya/thewhite">قالب سفید</a> اثر <a href="http://theyahya.com/">یحیی</a>
+        <a href="https://github.com/TheYahya/thewhite">قالب سبز</a> برگرفته از <a href="https://github.com/TheYahya/thewhite">قالب سفید</a>
     </p>
     </div>
 </footer>
