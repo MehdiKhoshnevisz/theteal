e61d9acfb6887f026efecf81de1b32e5412ad499
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-10-30 20:03:36 +0330

    update 404

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
-<?php get_header(); ?>
-<div class="container">
+<?php get_header('pure'); ?>
+<div class="content-center container">
     <div class="notf">
-        <h1><?php _e('Error 404'); ?></h1>
-        <p><?php _e('Page not found!');?></p>
+        <h1><?php _e('404'); ?></h1>
+        <p><?php _e('نداشتیم همچین چیزی!');?></p>
         <p><a href="<?php echo home_url(); ?>"><?php _e('Home page');?></a></p>
     </div> 
-<?php get_footer(); ?>
+<?php get_footer('pure'); ?>
